41c819026c53589cd23b38850d5520ed93ba9e00
elliotced <elliotceder@gmail.com>
2025-11-28 14:50:37 +0100

    Added edit functionality

--- app.rb
+++ app.rb
@@ -19,6 +19,22 @@ class App < Sinatra::Base
       erb(:"index")
     end
 
+    # Routen visar edit ToDo med id
+    get '/:id/edit' do | id |
+      @todo = db.execute('SELECT * FROM todos WHERE id = ?', id).first
+      erb(:"edit")
+    end
+
+    # Routen updaterar ToDo med id
+    post '/:id/update' do | id |
+      title = params["title"]
+      description = params["description"]
+
+      db.execute("UPDATE todos SET title =?, description=? WHERE id =?", [title, description, id])
+
+      redirect("/")
+    end
+
     # Routen tar bort ToDo med id
     post '/:id/delete' do | id |
       db.execute('DELETE FROM todos WHERE id=?', id)
